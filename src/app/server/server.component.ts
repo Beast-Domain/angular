081ad3bd32e62ac3ef5130b89084e92141cb35e6
ashraf kerawala <ashrafkerawala17@gmail.com>
2020-08-04 21:54:28 +0530

    [New]: Fresh start

---
+++ src/app/server/server.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-server',
+  templateUrl: './server.component.html',
+  styleUrls: ['./server.component.css']
+})
+export class ServerComponent implements OnInit {
+  title = "New Server";
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  createRange(number) {
+    var items: number[] = [];
+    for(var i=1; i <= number; i++) {
+      items.push(i);
+    }
+    return items;
+  }
+
+}
