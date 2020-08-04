3f74082ec7a33ef2b8b8d6ace59b1440b705d4db
ashraf kerawala <ashrafkerawala17@gmail.com>
2020-08-04 23:02:51 +0530

    [added]: jquery and new bootstrap

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ServerComponent } from './server/server.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'server',component:ServerComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
